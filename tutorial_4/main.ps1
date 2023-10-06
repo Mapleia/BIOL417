349969b4c3be9ea6c86be1076c3e530e89df32c8
Mapleia <kaede.ito2001@gmail.com>
2023-10-05 18:21:00 -0700

    added zip for tut4

--- tutorial_4/main.ps1
+++ tutorial_4/main.ps1
@@ -1,2 +1,2 @@
-Expand-Archive -Path ".\setup\Files_for_Tutorial-4.zip" -DestinationPath ".\tutorial4"
-.\.venv\Scripts\python.exe ".\partitionfinder-2.1.1\PartitionFinder.py" ".\tutorial4"
+Expand-Archive -Path ".\setup\Files_for_Tutorial-4.zip" -DestinationPath ".\3gene"
+.\.venv\Scripts\python.exe ".\partitionfinder-2.1.1\PartitionFinder.py" ".\3gene"
