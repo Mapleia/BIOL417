c0b8cf5f084bc2c1b2387c29f0be037fc4fd9dc8
Kaede Ito <mapleia01@gmail.com>
2023-10-27 04:52:45 -0700

    edit lab 4 scripts

--- lab_4/docker_run.ps1
+++ lab_4/docker_run.ps1
@@ -1,18 +1,20 @@
 # path to the 3gene directory
 # the zip is downloaded into the setup directory
 
-$3gene_dir = ".\lab_4\3gene"
+$3gene_dir = "${PWD}\lab_4\3gene"
 
 # check if 3gene directory exists
 $3gene_exists = Test-Path -PathType container $3gene_dir
 if (!$3gene_exists) {
-  Expand-Archive -Path ".\tutorial_4\setup\Files_for_Tutorial-4.zip" -DestinationPath $3gene_dir
+  Expand-Archive -Path "${PWD}\lab_4\setup\Files_for_Tutorial-4.zip" -DestinationPath $3gene_dir
 }
 
 # copy the partition_finder.cfg file into the 3gene directory
-Copy-Item -Path ".\tutorial_4\setup\partition_finder.cfg" -Destination ".\tutorial_4\3gene"
+Copy-Item -Path "${PWD}\lab_4\setup\partition_finder.cfg" -Destination "${PWD}\lab_4\3gene"
+
+docker build -f lab_4/Dockerfile -t biol417/partitionfinder:v1.1 .
 
 # run the partition finder
-docker run -v "${PWD}\tutorial_4\3gene:/app/3gene" --rm `
-  "biol417/partitionfinder:latest" `
-  python "partitionfinder-2.1.1//PartitionFinder.py" "./3gene"
+docker run -v "${PWD}\lab_4\3gene:/app/3gene" --rm `
+  "biol417/partitionfinder:v1.1" `
+  python "partitionfinder-2.1.1/PartitionFinder.py" -v -f "./3gene"
